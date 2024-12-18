8dce72761775961d2410e7d1688412520ee7c6c6
marianZamorano <mzamorano295@gmail.com>
2024-12-18 12:40:28 -0400

    en procesito

--- app/src/main/java/com/example/sinnombre/PuntajeActivity.kt
+++ app/src/main/java/com/example/sinnombre/PuntajeActivity.kt
@@ -1,20 +1,21 @@
 package com.example.sinnombre
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.sinnombre.databinding.ActivityPuntajeBinding
 
 class PuntajeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPuntajeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_puntaje)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityPuntajeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.botonAtras.setOnClickListener {
+            val intent = Intent(this, PantallaDeInicioActivity::class.java)
+            startActivity(intent)
         }
     }
 }
