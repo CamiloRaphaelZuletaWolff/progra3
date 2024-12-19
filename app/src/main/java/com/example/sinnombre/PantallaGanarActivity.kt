9acb064fc2da619e02ebf93e529aa9ebf70e1831
VALERIA <valemartinezsoria@gmail.com>
2024-12-18 22:49:50 -0400

    todo DE NUEVO

--- app/src/main/java/com/example/sinnombre/PantallaGanarActivity.kt
+++ app/src/main/java/com/example/sinnombre/PantallaGanarActivity.kt
@@ -4,33 +4,33 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sinnombre.databinding.ActivityPantallaganarBinding // Asegúrate de que el binding esté configurado correctamente
-import com.example.sinnombre.PantallaDeInicioActivity // Cambia al nombre completo del paquete donde esté declarada tu actividad
+import com.example.sinnombre.databinding.ActivityPantallaganarBinding
+import com.example.sinnombre.PantallaDeInicioActivity
 
 class PantallaGanarActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityPantallaganarBinding // Usar ViewBinding si lo configuraste correctamente
+    private lateinit var binding: ActivityPantallaganarBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Configurar ViewBinding
+
         binding = ActivityPantallaganarBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val botonSiguientenivel: ImageButton = findViewById(R.id.boton_siguiente_nivel)
         botonSiguientenivel.setOnClickListener {
-            // Redirigir a PantallaDeInicioActivity
+
             val intent = Intent(this, PantallaDeInicioActivity::class.java)
             startActivity(intent)
 
-            // Configurar el botón "Atrás"
+
             val botonAtras: ImageButton = findViewById(R.id.boton_inicio)
             botonAtras.setOnClickListener {
-                // Redirigir a PantallaDeInicioActivity
+
                 val intent = Intent(this, PantallaDeInicioActivity::class.java)
                 startActivity(intent)
-                finish() // Finaliza esta actividad
+                finish()
             }
         }
     }
--- app/src/main/java/com/example/sinnombre/PantallaPerderActivity.kt
+++ app/src/main/java/com/example/sinnombre/PantallaPerderActivity.kt
@@ -4,34 +4,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import com.example.sinnombre.databinding.ActivityPantallaganarBinding // Asegúrate de que el binding esté configurado correctamente
-import com.example.sinnombre.PantallaDeInicioActivity // Cambia al nombre completo del paquete donde esté declarada tu actividad
 import com.example.sinnombre.databinding.ActivityPantallaperderBinding
 
 class PantallaPerderActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityPantallaperderBinding // Usar ViewBinding si lo configuraste correctamente
+    private lateinit var binding: ActivityPantallaperderBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Configurar ViewBinding
+
         binding = ActivityPantallaperderBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val botonReiniciar: ImageButton = findViewById(R.id.boton_reiniciar)
         botonReiniciar.setOnClickListener {
-            // Redirigir a PantallaDeInicioActivity
+
             val intent = Intent(this, PantallaDeInicioActivity::class.java)
             startActivity(intent)
 
-            // Configurar el botón "Atrás"
+
             val botonAtras: ImageButton = findViewById(R.id.boton_inicio)
             botonAtras.setOnClickListener {
-                // Redirigir a PantallaDeInicioActivity
+
                 val intent = Intent(this, PantallaDeInicioActivity::class.java)
                 startActivity(intent)
-                finish() // Finaliza esta actividad
+                finish()
             }
         }
     }
